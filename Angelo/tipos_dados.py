8ff3b428b59499803662ac219631d94c3cd5f021
Ângelo Santos <angelovinicius123@gmail.com>
2024-05-09 19:46:11 -0300

    Alteração no arquivo tipos_dados.py

--- Angelo/tipos_dados.py
+++ Angelo/tipos_dados.py
@@ -1,14 +1,24 @@
-#Coisas basicas do Python
-
 #Pricipais tipos de dados utilizados
 
 #O tipo int nada mais é do que números inteiros, indicado para uso em contador por exemplo.
 inteiro = int(1)
 
-#O tipo float
+#O tipo float são os números reais, ex: 2,5. Porém ao digitarmos números reais
+#em diversas linguagens de programação, devemos nos orientar no uso do ponto
+#invés da vírgula, pois ao utilizar a vírgula o seu código exibirá uma mensagem de
+#erro.
 real = float(1.5)
 
+#O tipo boolean se trata do famoso V ou F, verdadeiro ou falso.
+#Onde True é verdadeiro e False é falso. Vale salientar que é obrigatório
+#o uso da letra maiúscula ao utilizar esse tipo de dado.
 booleano = True
+
+#O tipo string é utilizado para textos, diferente dos demais com ele 
+#você não precisa declarar o tipo ao declarar uma variável.
+#Ele sempre vem acompanhado de aspas simples ' ' ou aspas duplas " ".
 string = "Texto"
 
-print(inteiro, real, booleano, string)
+#Obs: existem outros tipos de dados como: complex, list, tuple, dict. . .
+#porém não pretendo aborda-lós, caso ache necessário entende-lós,
+#dê um Google :)
