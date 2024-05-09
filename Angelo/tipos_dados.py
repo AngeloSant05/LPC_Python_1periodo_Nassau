29f1a0bdef04420bdfb9d047e6cf3547599e9be2
Ângelo Santos <angelovinicius123@gmail.com>
2024-05-09 18:24:15 -0300

    Alterei o nome do basico.py para tipos_dados.py

---
+++ Angelo/tipos_dados.py
@@ -0,0 +1,14 @@
+#Coisas basicas do Python
+
+#Pricipais tipos de dados utilizados
+
+#O tipo int nada mais é do que números inteiros, indicado para uso em contador por exemplo.
+inteiro = int(1)
+
+#O tipo float
+real = float(1.5)
+
+booleano = True
+string = "Texto"
+
+print(inteiro, real, booleano, string)
